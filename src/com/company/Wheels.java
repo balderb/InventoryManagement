abeaa8174c43888474f5d430b29bdf4656cd73e5
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-12-01 12:27:54 +0100

    update wheels.java
    update wheels.java additional debugging

--- src/com/company/Wheels.java
+++ src/com/company/Wheels.java
@@ -1,36 +1,65 @@
-package com.company;
+package com.skillup;
 
 public class Wheels {
 
-    private static int totalTires;
-    private static int totalWideTires;
-    private static int totalStandardTires;
+    private static int totalWheelsCount;
+    private static int totalWideWheelsCount;
+    private static int totalNormalWheelCount;
 
     private boolean isWide;
+    private final int price;
 
-    public Wheels( boolean isWide) {
+    public Wheels(boolean isWide) {
         this.isWide = isWide;
-        if(isWide){
-            totalWideTires++;
-        }else {
-            totalStandardTires++;
+        price = decrementWheels(isWide);
+        if (price!=0){
+            totalWheelsCount--;
         }
-        totalTires++;
+    }
+
+    private int decrementWheels(boolean isWide){
+        if (isWide){
+            if(--totalWideWheelsCount<0){
+                System.err.println("Not enough Wide Wheels.");
+                totalWideWheelsCount++;
+            }else return 200;
+        }else {
+            if(--totalNormalWheelCount<0){
+                System.err.println("Not enough Wide Wheels.");
+                totalNormalWheelCount++;
+            }else return 100;
+        }
+        return 0;
+    }
+
+    public static void addWheels(int standardWheels, int wideWheels) {
+        if (standardWheels>=0&&wideWheels>=0){
+            totalNormalWheelCount+=standardWheels;
+            totalWideWheelsCount+=wideWheels;
+            totalWheelsCount+=(standardWheels+wideWheels);
+        }
+        else {
+            System.err.println("Error, negative value not allowed");
+        }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static int getTotalWheelsCount() {
+        return totalWheelsCount;
+    }
+
+    public static int getTotalWideWheelsCount() {
+        return totalWideWheelsCount;
+    }
+
+    public static int getTotalNormalWheelCount() {
+        return totalNormalWheelCount;
     }
 
     public boolean isWide() {
         return isWide;
-    }
-
-    public static int getTotalTires() {
-        return totalTires;
-    }
-
-    public static int getTotalWideTires() {
-        return totalWideTires;
-    }
-
-    public static int getTotalStandardTires() {
-        return totalStandardTires;
     }
 }
